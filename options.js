6ca9be56cb1e27c00cdb5fe4563df17493ab7eac
macarc <archiepiper@hotmail.com>
2020-12-16 17:06:20 +0000

    Fixed bug with maxing out storage writes

--- options.js
+++ options.js
@@ -1,4 +1,5 @@
-function updateUI(prefs) {
+let prefs = {};
+function initialiseUI() {
     for (const category in prefs) {
         if (! (prefs.hasOwnProperty(category))) continue;
         for (const attribute in prefs[category]) {
@@ -6,12 +7,11 @@ function updateUI(prefs) {
 
             const element = document.getElementById(category).getElementsByClassName(attribute)[0];
             (element.type === 'checkbox') ? element.checked = prefs[category][attribute] : element.value = prefs[category][attribute];
-
             element.addEventListener('change', function() {
                 let newPrefs = JSON.parse(JSON.stringify(prefs));
                 newPrefs[category][attribute] = (this.type === 'checkbox') ? this.checked : this.value;
                 savePrefs(newPrefs);
-                updateUI(newPrefs);
+                prefs = newPrefs;
             });
         }
     }
@@ -19,10 +19,12 @@ function updateUI(prefs) {
 
 document.addEventListener('DOMContentLoaded', async () => {
     const initialCSS = await getPrefs();
-    updateUI(initialCSS);
+    prefs = JSON.parse(JSON.stringify(initialCSS));
+
+    initialiseUI();
 
     document.querySelector('#reset').addEventListener('click', () => {
         savePrefs(initialCSS);
-        updateUI(initialCSS);
+        window.location.reload();
     });
 });
